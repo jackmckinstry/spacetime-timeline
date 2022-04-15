04482283d9738e5a43d374d1ae38fe7848dfaec3
Jack McKinstry <jack.mckinstry@colorado.edu>
2022-04-14 20:31:14 -0600

    second initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
 import './App.css';
 
 function App() {
+  const [bDate, setBDate] = useState("");
+  const [h1_Text, setHeading] = useState("Enter your birthday:");
+  const [h2_Text, setSubHeading] = useState("");
+  const [isMousedOver, setMouseOver] = useState(false);
+
+  function handle_change(event) {
+    setBDate(event.target.value);
+    console.log(event.target.value);
+    // console.log(event);
+    // console.log(event.target.type);
+    // console.log(event.target.placeholder);
+  }
+
+  function handleMouseOver() {
+    setMouseOver(!isMousedOver);
+  }
+
+  function handle_click(event) {
+    setHeading(bDate);
+    console.log(bDate);
+    if (bDate.includes("08-07")) setSubHeading("August 7? That's my birthday!");
+    else setSubHeading("");
+    event.preventDefault();
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={"./moon.png"} className="App-logo" alt="moon" />
       </header>
+
+      <body>
+        <h1> {h1_Text} </h1>
+        <h3> {h2_Text} </h3>
+        <form onSubmit={handle_click}>
+          <input
+            onChange={handle_change}
+            type="date"
+            //placeholder="What's your birthday?"
+            value={bDate}
+          />
+          <button
+            style={{ backgroundColor: isMousedOver ? "Aqua" : "white" }}
+            type="submit"
+            onMouseOver={handleMouseOver}
+            onMouseOut={handleMouseOver}
+          >
+            Submit
+          </button>
+        </form>
+      </body>
+
+      
     </div>
   );
 }
